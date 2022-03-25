b8dd34f80c7f50b33a8e1333df9ee6bc4e80372b
Josscoder <josscoder@hotmail.com>
2022-03-24 21:20:09 -0500

    feat: project finished

---
+++ StateSwitch.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace FSMgasm;
+
+class StateSwitch {
+	protected ?State $currentState = null;
+
+	public function changeState(State $newState): void {
+		$this->currentState->end();
+		$this->currentState = $newState;
+		$newState->start();
+	}
+
+	public function update(): void {
+		$this->currentState->update();
+	}
+}
